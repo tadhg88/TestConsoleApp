3b9796d12979d3e7e1b87f15eb53d4374d3f0ff4
timwhelan <tadhg88@gmail.com>
2010-05-28 20:39:58 +0100

    added class for timer

---
+++ ThreadTimer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestConsoleApp
+{
+    public class ThreadTimer
+    {
+        public static void WaitForTimer()
+        {
+            System.Threading.Thread.Sleep(2000);
+        }
+    }
+}
